8b42d7d6708b5375c1979762f401b2f242721805
hackuna885 <oliver885@hotmail.com>
2016-01-14 07:46:57 -0600

    Nicolás Romero 14012016
    Plantilla Web de Nicolás Romero

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Nicolás Romero 2016 - 2018 Trabajo y Resultados</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body class="homepage">
+	<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<div class="logo container">
+						<div>
+							<h1><a href="index.php" id="logo">H. Ayuntamiento de Nicolás Romero</a></h1>
+							<p>2016 - 2018</p>
+						</div>
+					</div>
+				</header>
+
+			<!-- Nav -->
+				<nav id="nav">
+					<ul>
+						<li class="current"><a href="index.php">Inicio</a></li>
+						<li>
+							<a href="#">Texto</a>
+							<ul>
+								<li><a href="#">Texto</a></li>
+								<li><a href="#">Texto</a></li>
+								<li>
+									<a href="#">Texto</a>
+									<ul>
+										<li><a href="#">Texto</a></li>
+										<li><a href="#">Texto</a></li>
+										<li><a href="#">Texto</a></li>
+										<li><a href="#">Texto</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Texto</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Texto</a></li>
+						<li><a href="#">Texto</a></li>
+						<li><a href="#">Texto</a></li>
+					</ul>
+				</nav>
+
+			<!-- Banner -->
+				<div id="banner-wrapper">
+					<section id="banner">
+						<!-- <h2>Título</h2>
+						<p>Gobierno de Trabajo y Resultados</p>
+						<a href="#" class="button">Texto</a> -->
+					</section>
+				</div>
+
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div id="main" class="container">
+						<div class="row 200%">
+							<div class="12u">
+
+								<!-- Highlight -->
+									<section class="box highlight">
+										<ul class="special">
+											<li><a href="#" class="icon fa-calendar-minus-o"><span class="label">Magnifier</span></a></li>
+											<li><a href="#" class="icon fa-camera"><span class="label">Tablet</span></a></li>
+											<li><a href="#" class="icon fa-users"><span class="label">Flask</span></a></li>
+											<li><a href="#" class="icon fa-envelope"><span class="label">Cog?</span></a></li>
+										</ul>
+										<header>
+											<h2>Título del Artículo</h2>
+											<p>Subtitulo del Artículo</p>
+										</header>
+										<p>
+											Texto del Artículo
+										</p>
+									</section>
+
+							</div>
+						</div>
+						<div class="row 200%">
+							<div class="12u">
+
+								<!-- Features -->
+									<section class="box features">
+										<h2 class="major"><span>Separador</span></h2>
+										<div>
+											<div class="row">
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic01.png" alt="" /></a>
+															<h3><a href="#">Título de la imagen</a></h3>
+															<p>
+																Texto de la Imagen.
+															</p>
+														</section>
+
+												</div>
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic01.png" alt="" /></a>
+															<h3><a href="#">Título de la imagen</a></h3>
+															<p>
+																Texto de la Imagen.
+															</p>
+														</section>
+
+												</div>
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic01.png" alt="" /></a>
+															<h3><a href="#">Título de la imagen</a></h3>
+															<p>
+																Texto de la Imagen.
+															</p>
+														</section>
+
+												</div>
+												<div class="3u 12u(mobile)">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic01.png" alt="" /></a>
+															<h3><a href="#">Título de la imagen</a></h3>
+															<p>
+																Texto de la Imagen.
+															</p>
+														</section>
+
+												</div>
+											</div>
+											<div class="row">
+												<div class="12u">
+													<ul class="actions">
+														<li><a href="#" class="button big">Boton Uno</a></li>
+														<li><a href="#" class="button alt big">Boton Dos</a></li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+						<div class="row 200%">
+							<div class="12u">
+
+								<!-- Blog -->
+									<section class="box blog">
+										<h2 class="major"><span>Separador</span></h2>
+										<div>
+											<div class="row">
+												<div class="9u 12u(mobile)">
+													<div class="content content-left">
+
+														<!-- Featured Post -->
+															<article class="box post">
+																<header>
+																	<h3><a href="#">Título del Blog</a></h3>
+																	<p>Subtitulo del Blog</p>
+																	<ul class="meta">
+																		<li class="icon fa-clock-o">Posteado hace 15 mins</li>
+																		<li class="icon fa-comments"><a href="#">8</a></li>
+																	</ul>
+																</header>
+																<a href="#" class="image featured"><img src="images/pic05.png" alt="" /></a>
+																<p>
+																	Texto del Blog.
+																</p>
+																<a href="#" class="button">Boton Uno D</a>
+															</article>
+
+													</div>
+												</div>
+												<div class="3u 12u(mobile)">
+													<div class="sidebar">
+
+														<!-- Archives -->
+															<ul class="divided">
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">Artículo</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">hace 6 horas</li>
+																			<li class="icon fa-comments"><a href="#">34</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">Artículo</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">hace 9 horas</li>
+																			<li class="icon fa-comments"><a href="#">27</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">Artículo</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">hace 20 horas</li>
+																			<li class="icon fa-comments"><a href="#">27</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">Artículo</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">hace 1 Día</li>
+																			<li class="icon fa-comments"><a href="#">27</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">Artículo</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock-o">hace 1 semana</li>
+																			<li class="icon fa-comments"><a href="#">27</a></li>
+																		</ul>
+																	</article>
+																</li>
+															</ul>
+															<a href="#" class="button alt">Más Artículos</a>
+
+													</div>
+												</div>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<footer id="footer" class="container">
+					<div class="row 200%">
+						<div class="12u">
+
+							<!-- About -->
+								<section>
+									<h2 class="major"><span>Separador</span></h2>
+									<p>
+										Texto
+									</p>
+								</section>
+
+						</div>
+					</div>
+					<div class="row 200%">
+						<div class="12u">
+
+							<!-- Contact -->
+								<section>
+									<h2 class="major"><span>Redes sociales</span></h2>
+									<ul class="contact">
+										<li><a class="icon fa-facebook" href="#"><span class="label">Facebook</span></a></li>
+										<li><a class="icon fa-twitter" href="#"><span class="label">Twitter</span></a></li>
+										<li><a class="icon fa-instagram" href="#"><span class="label">Instagram</span></a></li>
+										<li><a class="icon fa-youtube" href="#"><span class="label">Youtube</span></a></li>
+										<!-- <li><a class="icon fa-google-plus" href="#"><span class="label">Google+</span></a></li> -->
+									</ul>
+								</section>
+
+						</div>
+					</div>
+
+					<!-- Copyright -->
+						<div id="copyright">
+							<ul class="menu">
+								<li>&copy; Todos los derechos reservados</li><li>Creado por: <a href="#">H. Ayuntamiento de Nicolás Romero 2016 - 2018</a></li>
+							</ul>
+						</div>
+
+				</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+</body>
+</html>
