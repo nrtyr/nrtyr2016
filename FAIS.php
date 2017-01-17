5d0ed4b05618d2e87ec21ecf285ba602d82c11b0
Hackuna DELL XPS <nrtyr2016@gmail.com>
2017-01-17 08:21:38 -0600

    FAIS 2016 17012017
    Actualización del FAIS

---
+++ FAIS.php
@@ -0,0 +1,37 @@
+<?php 
+include('assets/inc/top.inc');
+include('assets/inc/nav.inc');
+ ?>
+
+				<header id="header">
+					<div class="logo container">
+						<div>
+							<h1><a href="#" id="logo">FAIS</a></h1>
+						</div>
+					</div>	
+				</header>
+				<article class ="bloqueArticulo">
+					<!-- <h2>Ley de Ingresos del Municipio 2016.</h2>
+					<br>
+					<p class="pArticulo">
+					I.- De la Información Financiera Relativa a la Elaboración de las Iniciativas de Ley de Ingresos y los Proyectos de Presupuesto de Egreso.
+					</p> -->
+					<div class="cuadroSuperCentrado">
+					<div class="cuadroArticulo">
+					<li><a href="pdf/FAIS/FAIS_2016.pdf">
+					<!-- <li><a href="http://nicolasromero.mx/nrtyr2016/index.php"> -->
+						<i class="fa fa-file-pdf-o"></i> FAIS 2016. </i>
+					</a></li>
+					
+					</div>
+					</div>
+					<br>
+					
+				</article>
+				<br>
+				<br>
+
+ <?php 
+include("assets/inc/transparencia.inc");
+include("assets/inc/footer.inc");
+  ?>
